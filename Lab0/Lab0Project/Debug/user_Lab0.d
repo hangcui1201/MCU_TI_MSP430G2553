29ced1c7363edb6499a32d478419ae2794604035
Hang <cuihang1201@gmail.com>
2018-03-31 17:57:56 -0500

    initial commit

---
+++ Lab0/Lab0Project/Debug/user_Lab0.d
@@ -0,0 +1,15 @@
+# FIXED
+
+user_Lab0.obj: C:/MCU_TI_MSP430G2553/Lab0/user_Lab0.c
+user_Lab0.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430g2553.h
+user_Lab0.obj: C:/ti/ccsv7/ccs_base/msp430/include/in430.h
+user_Lab0.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/intrinsics.h
+user_Lab0.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/intrinsics_legacy_undefs.h
+user_Lab0.obj: C:/MCU_TI_MSP430G2553/Lab0/UART.h
+
+C:/MCU_TI_MSP430G2553/Lab0/user_Lab0.c: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/intrinsics.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.7.LTS/include/intrinsics_legacy_undefs.h: 
+C:/MCU_TI_MSP430G2553/Lab0/UART.h: 
