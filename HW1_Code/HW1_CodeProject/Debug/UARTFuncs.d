869154d68453d043f64f16198467f24b6718f0db
Hang <cuihang1201@gmail.com>
2018-04-22 19:00:44 -0500

    Add HW

---
+++ HW1_Code/HW1_CodeProject/Debug/UARTFuncs.d
@@ -0,0 +1,21 @@
+# FIXED
+
+UARTfuncs.obj: C:/MCU_TI_MSP430G2553/HW1_Code/UARTfuncs.c
+UARTfuncs.obj: C:/tiCCS7/ccsv7/ccs_base/msp430/include/msp430g2553.h
+UARTfuncs.obj: C:/tiCCS7/ccsv7/ccs_base/msp430/include/in430.h
+UARTfuncs.obj: C:/tiCCS7/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h
+UARTfuncs.obj: C:/tiCCS7/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h
+UARTfuncs.obj: C:/tiCCS7/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdarg.h
+UARTfuncs.obj: C:/tiCCS7/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdio.h
+UARTfuncs.obj: C:/tiCCS7/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/linkage.h
+UARTfuncs.obj: C:/tiCCS7/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdarg.h
+
+C:/MCU_TI_MSP430G2553/HW1_Code/UARTfuncs.c: 
+C:/tiCCS7/ccsv7/ccs_base/msp430/include/msp430g2553.h: 
+C:/tiCCS7/ccsv7/ccs_base/msp430/include/in430.h: 
+C:/tiCCS7/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h: 
+C:/tiCCS7/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h: 
+C:/tiCCS7/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdarg.h: 
+C:/tiCCS7/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdio.h: 
+C:/tiCCS7/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/linkage.h: 
+C:/tiCCS7/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdarg.h: 
