8dc206c11e273b15102af4463f94cbc0076edeba
Hang <cuihang1201@gmail.com>
2018-06-10 23:26:40 -0500

    Add files

--- HW3_Code/HW3_CodeProject/Debug/UARTFuncs.d
+++ HW3_Code/HW3_CodeProject/Debug/UARTFuncs.d
@@ -1,21 +1,27 @@
 # FIXED
 
 UARTfuncs.obj: C:/MCU_TI_MSP430G2553/HW3_Code/UARTfuncs.c
-UARTfuncs.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430g2553.h
-UARTfuncs.obj: C:/ti/ccsv7/ccs_base/msp430/include/in430.h
-UARTfuncs.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h
-UARTfuncs.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h
-UARTfuncs.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdarg.h
-UARTfuncs.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdio.h
-UARTfuncs.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/linkage.h
-UARTfuncs.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdarg.h
+UARTfuncs.obj: C:/ti/ccsv8/ccs_base/msp430/include/msp430g2553.h
+UARTfuncs.obj: C:/ti/ccsv8/ccs_base/msp430/include/in430.h
+UARTfuncs.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics.h
+UARTfuncs.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics_legacy_undefs.h
+UARTfuncs.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/stdarg.h
+UARTfuncs.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/sys/_types.h
+UARTfuncs.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/sys/cdefs.h
+UARTfuncs.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/machine/_types.h
+UARTfuncs.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/stdio.h
+UARTfuncs.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/_ti_config.h
+UARTfuncs.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/linkage.h
 
 C:/MCU_TI_MSP430G2553/HW3_Code/UARTfuncs.c: 
-C:/ti/ccsv7/ccs_base/msp430/include/msp430g2553.h: 
-C:/ti/ccsv7/ccs_base/msp430/include/in430.h: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdarg.h: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdio.h: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/linkage.h: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdarg.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/stdarg.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/stdio.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/_ti_config.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/linkage.h: 
--- HW3_Code/HW3_CodeProject/Debug/subdir_rules.mk
+++ HW3_Code/HW3_CodeProject/Debug/subdir_rules.mk
@@ -8,14 +8,14 @@ SHELL = cmd.exe
 UARTfuncs.obj: C:/MCU_TI_MSP430G2553/HW3_Code/UARTfuncs.c $(GEN_OPTS) | $(GEN_HDRS)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: MSP430 Compiler'
-	"C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/bin/cl430" -vmsp -O0 --use_hw_mpy=none --include_path="C:/ti/ccsv7/ccs_base/msp430/include" --include_path="C:/ti/ccsv7/ccs_base/msp430/include" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include" --define=__MSP430G2553__ -g --printf_support=minimal --diag_warning=225 --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="UARTFuncs.d_raw" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/bin/cl430" -vmsp -O0 --use_hw_mpy=none --include_path="C:/ti/ccsv8/ccs_base/msp430/include" --include_path="C:/ti/ccsv8/ccs_base/msp430/include" --include_path="C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include" --define=__MSP430G2553__ -g --printf_support=minimal --diag_warning=225 --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="UARTFuncs.d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
 user_HW3_Code.obj: C:/MCU_TI_MSP430G2553/HW3_Code/user_HW3_Code.c $(GEN_OPTS) | $(GEN_HDRS)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: MSP430 Compiler'
-	"C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/bin/cl430" -vmsp -O0 --use_hw_mpy=none --include_path="C:/ti/ccsv7/ccs_base/msp430/include" --include_path="C:/ti/ccsv7/ccs_base/msp430/include" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include" --define=__MSP430G2553__ -g --printf_support=minimal --diag_warning=225 --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="user_HW3_Code.d_raw" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/bin/cl430" -vmsp -O0 --use_hw_mpy=none --include_path="C:/ti/ccsv8/ccs_base/msp430/include" --include_path="C:/ti/ccsv8/ccs_base/msp430/include" --include_path="C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include" --define=__MSP430G2553__ -g --printf_support=minimal --diag_warning=225 --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="user_HW3_Code.d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
